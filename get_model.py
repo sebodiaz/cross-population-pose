eeca58b41cd06971fc88f97dbeb4fa4ae22deb1b
sebodiaz <sdd@mit.edu>
2025-06-26 15:42:10 -0400

    trimmed some stuff

--- get_model.py
+++ get_model.py
@@ -1,4 +1,4 @@
-from models.small_unet import Unet, DualUNet
+from models.small_unet import Unet
 from models.big_unet import UNet3D
 
 def get_models(opts):
@@ -6,7 +6,6 @@ def get_models(opts):
     models = {
         "small_unet":   Unet(dimension=3, input_nc=1 if not opts.use_amix else 17, output_nc=opts.nJoints, num_downs=opts.depth, ngf=opts.nFeat),
         "big_unet":     UNet3D(in_features=1, n_features=opts.nFeat, out_features=opts.nJoints, n_pool=opts.depth),
-        "dual_unet":  DualUNet(),
     }
 
 
